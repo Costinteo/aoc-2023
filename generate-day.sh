b434707839dcb1dc5a8c62ea1e530f0b8c2773c5
Costinteo <39965799+Costinteo@users.noreply.github.com>
2023-12-06 23:32:40 +0200

    Add generic error & read lines in generate-day.sh

--- generate-day.sh
+++ generate-day.sh
@@ -4,16 +4,17 @@ echo "Generating day$1.rs..."
 
 cat << _END_OF_FILE > src/day$1.rs
 use std::fs;
-use std::io::{self, BufRead, BufReader};
+use std::io::{BufRead, BufReader};
 
 use crate::util;
 use crate::INPUT_FILEPATH;
 
-pub fn solve() -> Result<(), io::Error> {
+pub fn solve() -> Result<(), Box<dyn std::error::Error>> {
     let input_filename = format!("{}{}.txt", INPUT_FILEPATH, util::get_input_filename(file!()));
 
     let file = fs::File::open(&input_filename).expect(&format!("File {} not found!", input_filename));
     let reader = BufReader::new(file);
+	let lines: Vec<String> = reader.lines().map(|res| res.expect("Unable to read lines!")).collect();
 
     Ok(())
 }
